9da336651ff921a3e60bb7b31396cfb1be929086
triongroup <triongroup@gmail.com>
2016-10-23 03:19:45 +0300

    Update getRideEstimate.php

--- src/routes/getRideEstimate.php
+++ src/routes/getRideEstimate.php
@@ -51,7 +51,7 @@ $app->post('/api/Uber/getRideEstimate', function ($request, $response, $args) {
     }
 
 
-    if($post_data['args']['sandbox'] == 1) {
+    if(isset($post_data['args']['sandbox']) && $post_data['args']['sandbox'] == 1) {
         $query_str = 'https://sandbox-api.uber.com/v1/requests/estimate';
     } else {
         $query_str = 'https://api.uber.com/v1/requests/estimate';
