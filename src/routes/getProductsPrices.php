31fc38f2eb388cf89d08d77ba2af23afe288a2c1
triongroup <triongroup@gmail.com>
2016-10-23 03:16:34 +0300

    Update getProductsPrices.php

--- src/routes/getProductsPrices.php
+++ src/routes/getProductsPrices.php
@@ -44,7 +44,7 @@ $app->post('/api/Uber/getProductsPrices', function ($request, $response, $args) 
         $query['seat_count'] = $post_data['args']['seatCount'];
     }
 
-    if($post_data['args']['sandbox'] == 1) {
+    if(isset($post_data['args']['sandbox']) && $post_data['args']['sandbox'] == 1) {
         $query_str = 'https://sandbox-api.uber.com/v1/estimates/price';
     } else {
         $query_str = 'https://api.uber.com/v1/estimates/price';
