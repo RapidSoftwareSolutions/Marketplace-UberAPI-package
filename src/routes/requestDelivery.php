0571bd4f64767bd62c7b7809ba9b5ed95b89fc66
triongroup <triongroup@gmail.com>
2016-10-23 03:23:15 +0300

    Update requestDelivery.php

--- src/routes/requestDelivery.php
+++ src/routes/requestDelivery.php
@@ -162,7 +162,7 @@ $app->post('/api/Uber/requestDelivery', function ($request, $response, $args) {
     }    
 
 
-    if($post_data['args']['sandbox'] == 1) {
+    if(isset($post_data['args']['sandbox']) && $post_data['args']['sandbox'] == 1) {
         $query_str = 'https://sandbox-api.uber.com/v1/deliveries';
     } else {
         $query_str = 'https://api.uber.com/v1/deliveries';
