a7d8faca73c80c3e5d935c7b81e46b3f751c2b5a
triongroup <triongroup@gmail.com>
2016-10-23 03:10:42 +0300

    Update cancelCurrentRide.php

--- src/routes/cancelCurrentRide.php
+++ src/routes/cancelCurrentRide.php
@@ -25,7 +25,7 @@ $app->post('/api/Uber/cancelCurrentRide', function ($request, $response, $args) 
     $headers['Content-Type'] = 'application/json'; 
 
 
-    if($post_data['args']['sandbox'] == 1) {
+    if(isset($post_data['args']['sandbox']) && $post_data['args']['sandbox'] == 1) {
         $query_str = 'https://sandbox-api.uber.com/v1/requests/current';
     } else {
         $query_str = 'https://api.uber.com/v1/requests/current';
